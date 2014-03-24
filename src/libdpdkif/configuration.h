a06ab4c83bc724ca73112fba4d811771884311e6
Antti Kantee <pooka@iki.fi>
2014-03-24 12:47:04 +0000

    put dpdkif configurables in a separate configuration.h file

---
+++ src/libdpdkif/configuration.h
@@ -0,0 +1,22 @@
+/*
+ * Configurables.  Adjust these to be appropriate for your system.
+ */
+
+/* change blacklist parameters (-b) if necessary */
+static const char *ealargs[] = {
+	"if_dpdk",
+	"-b 00:00:03.0",
+	"-c 1",
+	"-n 1",
+};
+
+/* change PORTID to the one your want to use */
+#define IF_PORTID 0
+
+/* change to the init method of your NIC driver */
+#ifndef PMD_INIT
+#define PMD_INIT rte_pmd_init_all
+#endif
+
+/* Receive packets in bursts of 16 per read */
+#define MAX_PKT_BURST 16
